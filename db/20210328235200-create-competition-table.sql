6ca78124ae7b496a4bf03197f28094a80192f604
ChristDave <60493803+ChristDave@users.noreply.github.com>
2021-04-05 22:15:10 -0400

    Delete 20210328235200-create-competition-table.sql

--- db/20210328235200-create-competition-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE competition(
-        competitionid int NOT NULL,
-        name varchar(50),
-        venue varchar(50),
-        start_date_time DATETIME,
-        end_date_time DATETIME,
-        duration VARCHAR(50),
-        PRIMARY KEY (competitionid)
-);
